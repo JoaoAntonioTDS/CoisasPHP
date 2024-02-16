c7d84abc9f4886d909bbd4b54f6a01e2f622b214
Joao Antonio <134729023+JoaoAntonioTDS@users.noreply.github.com>
2024-02-16 15:43:01 -0300

    Create DobroTriplo.php

---
+++ DobroTriplo.php
@@ -0,0 +1,9 @@
+<?php
+$nmr = readline("Informe um numero: ");
+
+if($nmr > 0){
+echo "O dobro do seu número é " . $nmr*2; echo"\n";
+}
+if($nmr < 0){
+    echo "O triplo do seu número é " . $nmr*3; echo"\n";
+}
