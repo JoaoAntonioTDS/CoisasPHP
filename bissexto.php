91919f6139932e6dd06810d29e33a8d5f59d7167
Joao Antonio <134729023+JoaoAntonioTDS@users.noreply.github.com>
2024-02-16 15:42:42 -0300

    bissexto.php

---
+++ bissexto.php
@@ -0,0 +1,11 @@
+<?php
+
+for($i=1984; $i<=2024; $i++){
+
+    if($i % 4 == 0 and $i % 100 != 0){
+   
+        echo$i; echo "\n";} 
+        
+        else if ($i % 4 ==0 and $i % 400 == 0){
+            echo $i;  echo "\n";
+        } }
