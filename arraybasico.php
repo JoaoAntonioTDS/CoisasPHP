ac708ee3c3332d42c1cdfacae6bec44046efc90d
Joao Antonio <134729023+JoaoAntonioTDS@users.noreply.github.com>
2024-02-29 14:51:40 -0300

    arraybasico.php

---
+++ arraybasico.php
@@ -0,0 +1,13 @@
+<?php
+    
+    $vetor = array(2, 5, 7, 8, -1, 6);
+
+    for($i=0; $i<count($vetor); $i++){
+    echo $vetor[$i] . "\n";
+} 
+
+print "\n";
+
+foreach ($vetor as $num){
+echo $num . "\n";
+}
