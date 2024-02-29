f7a7c5d3bf9c486aae1fa511d54db1fa9bc5e743
Joao Antonio <134729023+JoaoAntonioTDS@users.noreply.github.com>
2024-02-29 15:28:25 -0300

    Update ArrayAssociativo.php

--- ArrayAssociativo.php
+++ ArrayAssociativo.php
@@ -17,3 +17,23 @@
     foreach($Casa as $num){
      echo$num . " | ";
     }echo "\n";
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+<?php
+    function imprimeVetor($vet){
+    foreach($vet as $v){
+        echo $v . " | ";
+       }echo "\n";
+    }
+    $Man = array("man1", "man2", "man3", "man4",  "man5");
+    $Girl = array("girl1", "girl2", "girl3", "girl4" , "girl5");
+    $Aluno = array("aluno1", "aluno2", "aluno3", "aluno4" , "aluno5");
+    $Casa = array("casa1", "casa2", "casa3", "casa4" , "casa5");
+
+imprimeVetor($Man);
+imprimeVetor($Girl);
+imprimeVetor($Aluno);
+imprimeVetor($Casa);
