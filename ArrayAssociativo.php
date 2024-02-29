65b40407d75a028382ed9b91c7045e6f84a1254a
Joao Antonio <134729023+JoaoAntonioTDS@users.noreply.github.com>
2024-02-29 15:20:38 -0300

    ArrayAssociativo.php

---
+++ ArrayAssociativo.php
@@ -0,0 +1,19 @@
+<?php
+    
+    $Man = array("man1", "man2", "man3", "man4",  "man5");
+    $Girl = array("girl1", "girl2", "girl3", "girl4" , "girl5");
+    $Aluno = array("aluno1", "aluno2", "aluno3", "aluno4" , "aluno5");
+    $Casa = array("casa1", "casa2", "casa3", "casa4" , "casa5");
+
+    foreach($Man as $num){
+     echo$num . " | ";
+    }echo "\n";
+    foreach($Girl as $num){
+     echo$num . " | ";
+    }echo "\n";
+    foreach($Aluno as $num){
+     echo$num . " | ";
+    }echo "\n";
+    foreach($Casa as $num){
+     echo$num . " | ";
+    }echo "\n";
