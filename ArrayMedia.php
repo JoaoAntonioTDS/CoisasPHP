5d7f1053eac72027fd88434b31967e0953a880dc
Joao Antonio <134729023+JoaoAntonioTDS@users.noreply.github.com>
2024-03-07 13:56:42 -0300

    ArrayMedia.php

---
+++ ArrayMedia.php
@@ -0,0 +1,10 @@
+<?php
+
+$ArrayDez = [];
+
+for ($i = 1; $i <= 10; $i++) {
+    $numero = readline("Informe o {$i}º número: ");
+    $ArrayDez[] = $numero;
+}
+$media = array_sum($ArrayDez) / count($ArrayDez);
+echo "A média dos valores informados é: " . $media;
