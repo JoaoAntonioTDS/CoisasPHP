a46101ee65b5e79cb4688aaeb8203948a6618a65
Joao Antonio <134729023+JoaoAntonioTDS@users.noreply.github.com>
2024-03-21 09:55:52 -0300

    ArrayMedia.php

--- ArrayMedia.php
+++ ArrayMedia.php
@@ -3,7 +3,7 @@
 $ArrayDez = [];
 
 for ($i = 1; $i <= 10; $i++) {
-    $numero = readline("Informe o {$i}º número: ");
+    $numero = readline("Informe o $i º número: ");
     $ArrayDez[] = $numero;
 }
 $media = array_sum($ArrayDez) / count($ArrayDez);
